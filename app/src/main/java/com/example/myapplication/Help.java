fd1cd0fe80cc1f8951359bb5190c362fdb2c4c0b
Helle <helle1926@hotmail.com>
2019-10-19 13:54:18 +0200

    Første commit -.-'

---
+++ app/src/main/java/com/example/myapplication/Help.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Help extends AppCompatActivity implements View.OnClickListener {
+    TextView tx;
+    Button buttonnn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        tx = findViewById(R.id.textViewwww);
+        buttonnn = findViewById(R.id.button4);
+        buttonnn.setOnClickListener(this);
+        tx.setText("Har du problemer med at ....");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonnn){
+            tx.setText("Orale");
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,7 +19,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText editText;
     Button button;
     ImageView imageView;
-    String indtastetBogstav = editText.getText().toString();
+    String indtastetBogstav;
+    Animation scaleAnimation, rotateAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +28,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
         textView.setText(galgelogik.getSynligtOrd());
-        textView2 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
         button = findViewById(R.id.button);
         button.setOnClickListener(this);
         imageView = findViewById(R.id.imageView);
         editText = findViewById(R.id.editText);
         editText.setOnClickListener(this);
+        scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_animation);
+        rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
     }
     @Override
     public void onClick(View view) {
+        if(view == button){
+        galgelogik.nulstil();
+    }
+        textView.setText(galgelogik.getSynligtOrd());
+        indtastetBogstav= editText.getText().toString();
         galgelogik.gætBogstav(indtastetBogstav);
-        billedeSkift();
         textView2.setText(galgelogik.getBrugteBogstaver().toString());
+        editText.setText("");
+        billedeSkift();
+        tabtEllerVundet();
+        errorMessage();
     }
 
+    public void errorMessage(){
+        if (indtastetBogstav.length() > 1){
+            editText.setError("Kun et bogstav!");
+            editText.setText("");
+        }
+        else if (galgelogik.getAntalForkerteBogstaver() == 6){
+            editText.setError("Gætter du forkert, taber du!");
+            editText.setText("");
+        }
+    }
+    public void tabtEllerVundet(){
+        if (galgelogik.erSpilletTabt()){
+            textView.setText(galgelogik.getOrdet());
+            textView.startAnimation(scaleAnimation);
+            textView2.setText("ØVVV! Du tabte!");
+            textView2.startAnimation(rotateAnimation);
+        }
+        else if(galgelogik.erSpilletVundet()){
+            textView.setText(galgelogik.getOrdet());
+            textView.startAnimation(scaleAnimation);
+            textView2.setText("Yaaay du vandt med, " + galgelogik.getAntalForkerteBogstaver() + ", fejl!");
+            textView2.startAnimation(rotateAnimation);
+        }
+    }
+
+    // Har fået error til halvt virke med rigtige bogstaver og med dobbeltbogstaver.
+    //TODO: Se om du kan få skærmbillede 2 til at virke
+    // TODO: Start forfra til at virke (check!)
+    // TODO: Hjælpe-skærm
+    //TODO musik når man har vundet eller tabt
+    //TODO: Andre billeder.
+
     public void billedeSkift(){
-        switch (galgelogik.getAntalForkerteBogstaver()){
+        int forkertebogstaver = galgelogik.getAntalForkerteBogstaver();
+        switch (forkertebogstaver){
             case 0:
                 imageView.setImageResource(R.drawable.galge);
+                break;
             case 1:
                 imageView.setImageResource(R.drawable.forkert1);
                 break;
@@ -49,15 +98,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case 3:
                 imageView.setImageResource(R.drawable.forkert3);
+                break;
             case 4:
                 imageView.setImageResource(R.drawable.forkert4);
+                break;
             case 5:
                 imageView.setImageResource(R.drawable.forkert5);
+                break;
             case 6:
                 imageView.setImageResource(R.drawable.forkert6);
-            case 7:
+                break;
             default:
-                imageView.setImageResource(R.drawable.galge);
         }
+
+
     }
 }
